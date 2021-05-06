410c8704367f6f412a7830eb22a56acee4d3fb4d
George Borsato <georginhoborsato@gmail.com>
2021-05-06 20:40:05 -0300

    feat method day_week

--- lib/date_problems.rb
+++ lib/date_problems.rb
@@ -25,4 +25,10 @@ class DateProblems
 
     "Year: #{calculated_year}, months: #{calculated_month}, Days: #{calculated_date}"
   end
+
+  def day_week(y, m, d)
+    t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ]
+    y = y - 1 if m < 3
+    (y + y / 4 - y / 100 + y / 400 + t[m-1] + d) % 7
+  end
 end
