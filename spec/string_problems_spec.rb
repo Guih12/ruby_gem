e213e842b2000681c6ed0d6b1a3ab40c8672b88b
George Borsato <georginhoborsato@gmail.com>
2021-05-06 20:53:33 -0300

    feat test units count_characters

--- spec/string_problems_spec.rb
+++ spec/string_problems_spec.rb
@@ -1,11 +1,13 @@
 require 'string_problems'
 
-RSpec.describe DateProblems do
+RSpec.describe StringProblems do
   let(:string_problems) {StringProblems.new }
   
   context "testing calculated characters" do
+    response = {upper: 5, down: 8, number: 4,special: 2}
+    string = '#GeeKs01fOr@gEEks07'
     it "return result quantity caracters: upper, down, number and special" do
-      expect(string_problems).to eq(response) 
+      expect(string_problems.count_characters(string)).to eq(response) 
     end
   end
 end
