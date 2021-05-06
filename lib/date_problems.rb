1be75728cb3203dbb00ffeac58999566a2730d60
George Borsato <georginhoborsato@gmail.com>
2021-05-06 20:34:24 -0300

    feat method find-age

--- lib/date_problems.rb
+++ lib/date_problems.rb
@@ -6,4 +6,23 @@ class DateProblems
     minute = add_minutes%60
     "#{hour}:#{minute}"
   end
+
+  def find_age(current_date, current_month, current_year, 
+    birth_date, birth_month, birth_year)
+  
+    month =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if birth_date > current_date
+      current_month = current_month - 1
+      current_date = current_date + month[birth_month-1]
+    end
+  
+  
+    if(birth_month > current_month)
+      calculated_date = current_date - birth_date
+      calculated_month = current_month - birth_date
+      calculated_year = current_year - birth_year
+    end
+
+    "Year: #{calculated_year}, months: #{calculated_month}, Days: #{calculated_date}"
+  end
 end
