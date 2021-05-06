983d3465e5c029c318b9832eb610eabe6cccae38
George Borsato <georginhoborsato@gmail.com>
2021-05-06 20:34:57 -0300

    feat test unit find age

--- spec/date_problems_spec.rb
+++ spec/date_problems_spec.rb
@@ -6,8 +6,23 @@ RSpec.describe DateProblems do
     time = "21:39"
     k = 43
     response = "22:22"
-    it "should returnr sum time " do
+    it "should return sum time " do
       expect(date_problems.sum_time(time, k)).to eq(response) 
+    end
+  end
+  
+  context "testing find age" do
+    let(:current_date) { 7 }
+    let(:current_month) { 12 }
+    let(:current_year) { 2017 }
+    
+    let(:birth_date) { 16 }
+    let(:birth_month) { 12 }
+    let(:birth_year) { 2009 }
+    response = "Year: 8, months: -5, Days: 22"
+    it "should return presente age " do
+      expect(date_problems.find_age(current_date, current_month, current_year,
+        birth_date, birth_month, birth_year)).to eq(response)   
     end
   end
   
