8c0569cbf70e1e192e342eba61c7ec125f51ecc3
George Borsato <georginhoborsato@gmail.com>
2021-05-06 05:29:45 -0300

    feat test units of sorting_alogiritms

---
+++ spec/sorting_algorithms_spec.rb
@@ -0,0 +1,31 @@
+require 'sorting_algorithms'
+
+RSpec.describe SortingAlgorithms do
+	let(:arr) { [2,10,20,30,50,60,120] }
+	let(:arr_ordenate) { [2,10,20,30,50,60,120] } 
+	let(:sorting_algorithm) { SortingAlgorithms.new(arr) }
+	
+	context "testing bubble sort" do
+		it "should return array ordenate" do
+			expect(sorting_algorithm.bubble_sort).to eql(arr_ordenate) 
+		end
+	end
+
+	context "testing quick_sort" do
+    it "should return arr ordernate" do
+      expect(sorting_algorithm.quick_sort(arr, 0, arr.length-1)).to eql(arr_ordenate) 
+    end
+  end
+
+	context "testing select_sort" do
+    it "should return arr ordenate" do
+      expect(sorting_algorithm.select_sort).to eql(arr_ordenate) 
+    end
+  end
+
+	context "testing merge_sort" do
+    it "should return arr ordernate" do
+      expect(sorting_algorithm.merge_sort(arr)).to eql(arr_ordenate) 
+    end
+  end
+end
